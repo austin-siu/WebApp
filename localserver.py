7d6810ef32d5f4fc998ea35115a6cafde27e675b
Austin Siu <austinsiu2458@gmail.com>
2019-04-15 13:48:07 -0400

    First commit

---
+++ localserver.py
@@ -0,0 +1,30 @@
+''' 
+Local Server Holding Projects
+'''
+
+from flask import Flask, url_for, request, render_template, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
