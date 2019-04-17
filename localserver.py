2ae673bbb3678c6392679aede2eecd3d7d00e63e
Austin Siu <austinsiu2458@gmail.com>
2019-04-16 22:12:20 -0400

    login sessions

--- localserver.py
+++ localserver.py
@@ -1,9 +1,11 @@
 ''' 
 Local Server Holding Projects
 '''
+from flask import Flask, url_for, request, render_template, redirect, session, flash
+import os 
 
-from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 @app.route('/')
 def layout():
@@ -11,7 +13,10 @@ def layout():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -19,12 +24,13 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            session['logged_in'] = True
         else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+            flash('Invalid Credentials. Please try again.')       
+    return home() 
 
+
+  
 
