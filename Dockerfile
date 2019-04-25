52ae60e110baa17cc5386d90e1fc6ee880fba71f
Austin Siu <austinsiu2458@gmail.com>
2019-04-25 13:06:25 -0400

    Minor changes

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
-FROM ubuntu:16.04
+FROM ubuntu:18.04
 
 RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev
+    apt-get install -y python3-pip python3-dev
+
+WORKDIR /opt/
 
 # We copy just the requirements.txt first to leverage Docker cache
-COPY ./requirements.txt /Desktop/app/requirements.txt
+COPY ./requirements.txt ./
+RUN pip3 install -r requirements.txt
 
-WORKDIR /Desktop/app
+RUN chmod -R 666 /opt/
 
-RUN pip install -r requirements.txt
+COPY . .
 
-COPY . /Desktop/app
-
-ENTRYPOINT [ "python" ]
+ENTRYPOINT [ "python3" ]
 
 CMD [ "localserver.py" ]
--- localserver.py
+++ localserver.py
@@ -2,8 +2,7 @@
 Local Server Holding Projects
 '''
 import os
-
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -29,26 +28,30 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+'''Load known user.'''
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+'''Login form.'''
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
+'''Create new user form.'''
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
     password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-
+'''General page allowing to user to login or create new user.'''
 @app.route('/')
 def index():
     return render_template('index.html')
 
+'''Logging into known user account'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -62,6 +65,7 @@ def login():
         return '<h1>Invalid username or password</h1>'
     return render_template('login.html', form=form)
 
+'''Defining posts specific to specified user.'''
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -72,6 +76,7 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
+'''Create a new user.'''
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
@@ -86,11 +91,13 @@ def signup():
 
     return render_template('signup.html', form=form)
 
+'''Presents user with their page of known information.'''
 @app.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('dashboard.html', name=current_user.username)
 
+'''Logout Function.'''
 @app.route('/logout')
 @login_required
 def logout():
