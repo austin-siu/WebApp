cf03ca7509d93926af3c24fefafcef3739f1319a
Austin Siu <austinsiu2458@gmail.com>
2019-04-27 20:43:16 -0400

    Pagination, Posts displayed in new to old order, Seeing other users' posts

--- server/routes.py
+++ server/routes.py
@@ -13,7 +13,8 @@ def index():
 
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -132,3 +133,11 @@ def delete_post(post_id):
     db.session.commit()
     flash('Deleted!', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+ 
